a821c30af63f1e8db97a33aa9e291cc2ff55582a
Bernd Stahlbock <stahlbock@gmail.com>
2023-04-09 12:06:49 +0000

    fix installer

--- install.sh
+++ install.sh
@@ -3,13 +3,15 @@ set -x
 
 DRIVERNAME=dbus-generic-mqtt-pvinverter
 
+#handle read only mounts
+sh /opt/victronenergy/swupdate-scripts/remount-rw.sh
+
 #install
 rm -rf /opt/victronenergy/service/$DRIVERNAME
-rm -rf /opt/victronenergy/service-templates/$DRIVERNAME
 rm -rf /opt/victronenergy/$DRIVERNAME
 mkdir /opt/victronenergy/$DRIVERNAME
 cp -f /data/etc/$DRIVERNAME/* /opt/victronenergy/$DRIVERNAME &>/dev/null
-cp -rf /data/etc/$DRIVERNAME/service /opt/victronenergy/service-templates/$DRIVERNAME
+cp -rf /data/etc/$DRIVERNAME/service /opt/victronenergy/service/$DRIVERNAME
 
 #restart if running
 pkill -f "python .*/$DRIVERNAME.py"
@@ -21,3 +23,6 @@ if [ ! -f $filename ]; then
     chmod 755 $filename
 fi
 grep -qxF "sh /data/etc/$DRIVERNAME/install.sh" $filename || echo "sh /data/etc/$DRIVERNAME/install.sh" >> $filename
+
+# also set link directly to get it started by svscan at install time
+ln -s /data/etc/$DRIVERNAME/service /service/$DRIVERNAME
